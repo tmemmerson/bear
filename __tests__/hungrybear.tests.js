6c630f6bb3971935bfb97dff2b61b6a22bc3a8ce
Tristan Emmerson <tristan@stickerslug.com>
2020-06-29 10:03:21 -0700

    GREEN flag very hungry when food drops below zero.
    co-authored by Tristan Emmerson<tristan@stickerslug.com>
    co-authored-by: Micheal Hansen <micheal.hansen@gmail.com>.

--- __tests__/hungrybear.tests.js
+++ __tests__/hungrybear.tests.js
@@ -15,11 +15,16 @@ describe('Fuzzy Wuzzy', ()=>{
 
   test('should have a name and a food level of 10 when created', ()=>{
     expect(fuzzy.name).toEqual('Fuzzy');
-    expect(fuzzy.foodlevel).toEqual(10);
+    expect(fuzzy.foodLevel).toEqual(10);
   });
 
   test('should have a food level of 7 after 3001 milliseconds', ()=>{
     jest.advanceTimersByTime(3001);
-    expect(fuzzy.foodlevel).toEqual(7);
+    expect(fuzzy.foodLevel).toEqual(7);
   });
+
+  test('should get very hungry if the food level drops below zero', function(){
+    fuzzy.foodLevel = 0;
+    expect(fuzzy.didYouGetEaten()).toEqual(true);
+  })
 });
